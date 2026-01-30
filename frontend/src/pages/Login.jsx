b86a3055096ac15c9c57bbd784f2b9ba817635a9
KandlaguntaVenkataSivaNiranjanReddy <117029570+KandlaguntaVenkataSivaNiranjanReddy@users.noreply.github.com>
2026-01-30 12:00:23 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,22 +1,42 @@
+import { useState } from "react";
 import axios from "../api/axios";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const login = async () => {
-    const res = await axios.post("/auth/login", {
-      email: "test@test.com",
-      password: "123456"
-    });
-    localStorage.setItem("token", res.data.token);
-    navigate("/dashboard");
+    try {
+      const res = await axios.post("/auth/login", { email, password });
+      localStorage.setItem("token", res.data.token);
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Login failed");
+    }
   };
 
   return (
-    <div style={{ background: "#0f172a", height: "100vh", color: "white", padding: 40 }}>
-      <h1>KK FUNDA – DevOps Training</h1>
+    <div style={{ padding: 40 }}>
+      <h2>KK FUNDA – Login</h2>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      /><br /><br />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      /><br /><br />
+
       <button onClick={login}>Login</button>
+      <br /><br />
+      <button onClick={() => navigate("/register")}>Signup</button>
     </div>
   );
 }
