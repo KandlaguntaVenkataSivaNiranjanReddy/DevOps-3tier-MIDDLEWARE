68ba16fa4ec2c8fd182559ce5495eefa79ecb3b1
KandlaguntaVenkataSivaNiranjanReddy <117029570+KandlaguntaVenkataSivaNiranjanReddy@users.noreply.github.com>
2026-02-15 16:42:00 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Courses from "./pages/Courses";
 
-export default function App() {
-  return (
+export default function App(){
+  return(
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/courses" element={<Courses />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/courses" element={<Courses/>} />
       </Routes>
     </BrowserRouter>
   );
