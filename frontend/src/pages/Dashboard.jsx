eae815e357bc06799e584b0097983460a55497b4
KandlaguntaVenkataSivaNiranjanReddy <117029570+KandlaguntaVenkataSivaNiranjanReddy@users.noreply.github.com>
2026-02-15 12:46:43 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,22 @@
 import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
-export default function Dashboard() {
+export default function Dashboard(){
   const navigate = useNavigate();
-  return (
-    <div>
-      <h2>Welcome to KK FUNDA</h2>
-      <button onClick={() => navigate("/courses")}>View Courses</button>
+
+  return(
+    <>
+    <Navbar/>
+    <div className="container">
+      <div className="card" style={{textAlign:"center"}}>
+        <h1>Welcome to KK FUNDA</h1>
+        <p style={{margin:"15px 0"}}>Your DevOps Learning Platform</p>
+
+        <button onClick={()=>navigate("/courses")}>
+          Browse Courses
+        </button>
+      </div>
     </div>
+    </>
   );
 }
